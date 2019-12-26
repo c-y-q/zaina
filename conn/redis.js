d1fc3c9b5d2d8aae224132d55b1661597dbc48ea
eggsy.cao <eggsy.cao@pictureworks.biz>
2019-12-26 18:09:42 +0800

    mongodb,mysql,redis连接成功

--- conn/redis.js
+++ conn/redis.js
@@ -1,6 +1,3 @@
 const Redis = require("ioredis");
-const cache =
-  process.env.NODE_ENV == "prod"
-    ? new Redis.Cluster(config.redis)
-    : new Redis(config.redis);
+const cache = process.env.NODE_ENV == "prod" ? new Redis.Cluster(config.redis) : new Redis(config.redis);
 module.exports = cache;
