3b83786c13b80a55c6f49ea86d08d9c500fb4c0e
xiaomi233 <xiaomi23333@126.com>
2019-12-28 17:50:12 +0800

    微信犬证绑定

--- router/petInfo.js
+++ router/petInfo.js
@@ -1,4 +1,4 @@
-const petService = require('../service/petRegInfo');
+const petService = require('../service/petInfo');
 router.post('/getPetInfo', async (req, res, next) => {
     const phone = req.user.account;
     const result = await petService.getPetInfo(phone);
@@ -7,4 +7,43 @@ router.post('/getPetInfo', async (req, res, next) => {
         result
     })
 })
+
+router.post("/directBindDogRegNum", async (req, res) => {
+    const phone = req.user.account;
+
+    let isfree = await petService.isPetMaster(phone);
+      if (isfree.length > 0) {
+        console.log("验证通过!");
+      } else {
+        res.json({
+          status: 10011,
+          respMsg: "验证失败!"
+        });
+      }
+    const flag = await petService.isBinwxRef(
+      isfree[0].dogRegNum,
+      isfree[0].id,
+    );
+    if (flag.length > 0) {
+      throw {
+        status: 10011,
+        respMsg: " 已绑定过该号码，请勿重复绑定 !"
+      };
+    }
+    // const judePetExists = await petService.judePetExists(isfree[0].id);
+    // if (!judePetExists) {
+    //   throw {
+    //     status: 10011,
+    //     respMsg: " the dog not exists !"
+    //   };
+    // }
+    await petService.directBindDogRegNum(
+      isfree[0].id,
+      isfree[0].dogRegNum
+    );
+    res.json({
+      status: 200,
+      respMsg: "bind success !"
+    });
+  });
 module.exports = router;
--- service/petInfo.js
+++ service/petInfo.js
@@ -15,3 +15,42 @@ exports.getPetInfo = async (phone) => {
     db.close();
     return tools.toTuoFeng(result);
 }
+
+exports.isPetMaster = async (phone) => {
+  const db = await mysqlUtil();
+  const sql = ` SELECT r.* FROM pet_register_info r LEFT JOIN pet_master m ON r.master_id = m.id WHERE m.contact_phone = ? and r.pet_state in (0,1,3) `;
+  const result = await db.query(sql, [phone]);
+  db.close();
+  return tools.toTuoFeng(result);
+};
+
+exports.isBinwxRef = async (dogRegNum, dogRegId) => {
+  const db = await mysqlUtil();
+  const sql = ` select id from  wx_binding_petinf where  dog_reg_num = ? and pet_reg_id = ? `;
+  const result = await db.query(sql, [
+      dogRegNum,
+      dogRegId
+  ]);
+  db.close();
+  return tools.toTuoFeng(result);
+};
+
+exports.judePetExists = async id => {
+  const db = await mysqlUtil();
+  const sql = `select id from pet_register_info where id = ? `;
+  const result = await db.query(sql, [id]);
+  db.close();
+  return tools.toTuoFeng(result);
+};
+
+exports.directBindDogRegNum = async (petRegId, dogRegNum) => {
+  const db = await mysqlUtil();
+  const wx_pet_ref_sql = " insert into wx_binding_petinf set ? ";
+  const wxPubPetInfRel = {
+      pet_reg_id: petRegId,
+      dog_reg_num: dogRegNum
+  };
+  const result = await db.query(wx_pet_ref_sql, wxPubPetInfRel);
+  db.close();
+  return result;
+};
