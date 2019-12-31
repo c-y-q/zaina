073c7091f1c69432ec61b45e9dcc3f6eecb93a88
eggsy.cao <eggsy.cao@pictureworks.biz>
2019-12-31 17:46:51 +0800

    小贾宠物绑定犬证代码合并

--- router/petInfo.js
+++ router/petInfo.js
@@ -7,4 +7,42 @@ router.post('/getPetInfo', async (req, res, next) => {
         result
     })
 })
+
+router.post("/directBindDogRegNum", async (req, res) => {
+    const phone = req.user.account;
+    let isfree = await petService.isPetMaster(phone);
+      if (isfree.length > 0) {
+        log("验证通过!");
+      } else {
+        res.json({
+          status: 405,
+          respMsg: "验证失败!"
+        });
+      }
+    const flag = await petService.isBinwxRef(
+      isfree[0].dogRegNum,
+      isfree[0].id,
+    );
+    if (flag.length > 0) {
+      throw {
+        status: 405,
+        respMsg: " 已绑定过该号码，请勿重复绑定 !"
+      };
+    }
+    const judePetExists = await petService.judePetExists(isfree[0].id);
+    if (!judePetExists) {
+      throw {
+        status: 405,
+        respMsg: " the dog not exists !"
+      };
+    }
+    await petService.directBindDogRegNum(
+      isfree[0].id,
+      isfree[0].dogRegNum
+    );
+    res.json({
+      status: 200,
+      respMsg: "bind success !"
+    });
+  });
 module.exports = router;
