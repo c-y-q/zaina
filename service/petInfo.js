683a47e467bd5646615cfd523433964493d6d4d7
eggsy.cao <eggsy.cao@pictureworks.biz>
2020-01-04 10:54:31 +0800

    merge userId

--- service/petInfo.js
+++ service/petInfo.js
@@ -1,17 +1,36 @@
 exports.getPetInfo = async (phone) => {
     const db = await mysqlUtil();
-    const sql = ` select 
-                    replace(p.pet_photo_url, '${config.pet.replaceImgPath}', '${config.pet.imgHttp}')  pet_photo_url,
-                    p.gender,p.breed,p.coat_color, p.id,m.id_number,p.dog_reg_num,
-                    m.real_name,m.residential_address,m.contact_phone
-                   from 
-                      pet_master m, pet_register_info p,sys_branch s
-                    where
-                    p.area_code = s.code
-                    and m.id = p.master_id 
-                    and p.pet_state in(1,3)
-                    and m.contact_phone = ? `;
+    const sql = ` SELECT
+    REPLACE ( p.pet_photo_url, '${config.pet.replaceImgPath}', '${config.pet.imgHttp}' ) pet_photo_url,
+    p.pet_name,
+    p.gender,
+    p.birthday,
+    p.area_code,
+    p.breed,
+    p.coat_color,
+    p.id,
+    p.dog_reg_num,
+    m.id_number,
+    m.real_name,
+    m.residential_address,
+    m.contact_phone 
+  FROM
+    pet_master m,
+    pet_register_info p,
+    sys_branch s 
+  WHERE
+    p.area_code = s.CODE 
+    AND m.id = p.master_id 
+    AND p.pet_state IN ( 1, 3 ) 
+    AND m.contact_phone = ? `;
     const result = await db.query(sql, [phone]);
+    for (let i = 0; i < result.length; i ++) {
+      result[i].id_number = result[i].id_number.replace(
+        /^(.{6})(?:\d+)(.{4})$/,
+        "$1********$2"
+      );
+      result[i].contact_phone = result[i].contact_phone.replace(/(\d{3})\d{4}(\d{4})/, "$1****$2");
+    }
     db.close();
     return tools.toTuoFeng(result);
 }
