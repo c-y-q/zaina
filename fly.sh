54bd68d7f975234792017779d38d07de70f28ec7
eggsy.cao <eggsy.cao@pictureworks.biz>
2020-01-08 09:32:32 +0800

    启动变量判断.sh

--- fly.sh
+++ fly.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #启动是传入prod或者dev
-echo "the app start enviroment is $1"
-pm2 start ./ecosystem.config.js --env $1
+str=""
+if test -z $1; then
+    str="dev"
+else
+    str=$1
+fi
+echo "the app start enviroment is $str"
+pm2 start ./ecosystem.config.js --env $str
