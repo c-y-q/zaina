4fa92bc4d96320704ae032372e85a95229bd6570
eggsy.cao <eggsy.cao@pictureworks.biz>
2020-01-16 14:06:31 +0800

    msg

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,23 @@
 module.exports = {
-    apps: [{
-        name: "zaina",
-        script: "./index.js",
-        watch: true,
-        env_dev: {
-            "PORT": 7006,
-            "NODE_ENV": "dev"
-        },
-        env_prod: {
-            "PORT": 7003,
-            "NODE_ENV": "prod",
-        }
-    }]
-}
+  apps: [
+    {
+      name: "zaina",
+      script: "./index.js",
+      watch: true,
+      ignore_watch: ["./logs"],
+      log_date_format: "YYYY/MM/DD HH:mm:ss",
+      max_memory_restart: "1G",
+      error_file: `./logs/zaina-err.log`,
+      out_file: `./logs/zaina-out.log`,
+      log_file: `./logs/zaina-combined.log`,
+      env_dev: {
+        PORT: 7006,
+        NODE_ENV: "dev"
+      },
+      env_prod: {
+        PORT: 7003,
+        NODE_ENV: "prod"
+      }
+    }
+  ]
+};
--- index.js
+++ index.js
@@ -10,88 +10,94 @@ const app = express();
 const routers = require('./router/index');
 app.use(logger("dev"));
 app.all("*", function (req, res, next) {
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Credentials", true);
-    res.set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT ,OPTIONS");
-    // if (req.method.toLowerCase() == "options") res.send(200);
-    // else 
-    next();
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Credentials", true);
+  res.set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT ,OPTIONS");
+  // if (req.method.toLowerCase() == "options") res.send(200);
+  // else 
+  next();
 });
 app.use(express.json()).use(
-    express.urlencoded({
-        extended: false
-    })
+  express.urlencoded({
+    extended: false
+  })
 );
 app.use(cookieParser());
 app.use(
-    bodyParser.urlencoded({
-        limit: "100mb",
-        extended: false
-    })
+  bodyParser.urlencoded({
+    limit: "100mb",
+    extended: false
+  })
 ).use(
-    bodyParser.raw({
-        limit: "100mb"
-    })
+  bodyParser.raw({
+    limit: "100mb"
+  })
 ).use(
-    bodyParser.text({
-        limit: "100mb"
-    })
+  bodyParser.text({
+    limit: "100mb"
+  })
 );
 
 /**
  * 将参数接收方式统一改为req.body
  */
 app.use(function (req, res, next) {
-    req.body = req.method == 'GET' ? req.query : req.method == 'POST' ? req.body : req.param;
-    log(38, req.body)
-    next();
+  req.body = req.method == 'GET' ? req.query : req.method == 'POST' ? req.body : req.param;
+  req.token = req.headers.token || req.body.token || req.query.token || '';
+  let str = {
+    ip: req.ip,
+    token: req.token || '',
+    '请求参数': req.body
+  }
+  log(38, str)
+  next();
 })
 
 /**
  * 校验token
  */
 app.use(async function (req, res, next) {
-    if (config.noToken.includes(req.path)) {
-        next()
-    } else {
-        try {
-            const token = req.headers.token || req.body.token || req.query.token;
-            const tokenObj = tools.verifyToken(token, cert.public);
-            log(52, tokenObj)
-            const userName = tokenObj && tokenObj.audience || '';
-            const redisUserInfo = await cache.get(userName);
-            const userStr = redisUserInfo && JSON.parse(redisUserInfo); //tokenObj.visitIP == userStr.visitIP && userStr.visitIP == req.ip && 
-            if (!redisUserInfo || !(tokenObj.audience == userStr.audience && tokenObj.uuid == userStr.uuid)) {
-                let err = tools.throwError(403, 'token is wrong !');
-                next(err);
-            } else {
-                req.user = userStr;
-                next();
-            }
-        } catch (err) {
-            next(err)
-        }
+  if (config.noToken.includes(req.path)) {
+    next()
+  } else {
+    try {
+      const token = req.token;
+      const tokenObj = tools.verifyToken(token, cert.public);
+      log(52, tokenObj)
+      const userName = tokenObj && tokenObj.audience || '';
+      const redisUserInfo = await cache.get(userName);
+      const userStr = redisUserInfo && JSON.parse(redisUserInfo); //tokenObj.visitIP == userStr.visitIP && userStr.visitIP == req.ip && 
+      if (!redisUserInfo || !(tokenObj.audience == userStr.audience && tokenObj.uuid == userStr.uuid)) {
+        let err = tools.throwError(403, 'token is wrong !');
+        next(err);
+      } else {
+        req.user = userStr;
+        next();
+      }
+    } catch (err) {
+      next(err)
     }
+  }
 })
 routers(app);
 app.use(function (res, req, next) {
-    if (res.path.indexOf('/favicon.ico') != -1) next();
-    let err = tools.throwError(404, 'Not Found ');
-    next(err);
+  if (res.path.indexOf('/favicon.ico') != -1) next();
+  let err = tools.throwError(404, 'Not Found ');
+  next(err);
 });
 app.use(function (err, req, res, next) {
-    let errMsg = {
-        status: err.status || 500,
-        router: req.path,
-        msg: err.message
-    }
-    process.env.NODE_ENV == 'dev' ? errMsg.error = err.stack : errMsg.error = err.message;
-    res.json(errMsg);
-    log(83, errMsg);
+  let errMsg = {
+    status: err.status || 500,
+    router: req.path,
+    msg: err.message
+  }
+  process.env.NODE_ENV == 'dev' ? errMsg.error = err.stack : errMsg.error = err.message;
+  res.json(errMsg);
+  log(83, errMsg);
 });
 const server = http.createServer(app);
 server.listen(process.env.PORT || config.port);
 server.on("listening", function () {
-    const addr = server.address();
-    log(`app listen on port ${addr.port}....... 当前运行的环境是：${process.env.NODE_ENV}`);
+  const addr = server.address();
+  log(`app listen on port ${addr.port}....... 当前运行的环境是：${process.env.NODE_ENV}`);
 });
